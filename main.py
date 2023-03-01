94ba17e8e75c734dfd6c2f1cf20e1f39df7e19cf
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-03-01 21:37:07 +0000

    Day 1 Project

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ print( "Welcome to the Band Name Generator.")
 city = input("What's name of the city you grew up in?\n")
 #3. Ask the user for the pet name. Also use the \n to create the answer on a new line.
 pet = input("What's your pet's name?\n")
-#4. I use city and pet as variable to hold the value for both input. Combine both value city and pet and the reault their band name.
+#4. I use city and pet as variable to hold the value for both input. Combine both value city and pet and that reault their band name.
 print("Your band name could be "  + city+ " " +pet)
